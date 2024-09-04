fe82ff19de945252664f9c809b50893223487677
thisisthekap <christian.kapplmueller@tonestro.com>
2024-09-04 18:35:33 +0200

    initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace mauiiosloggingissue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
